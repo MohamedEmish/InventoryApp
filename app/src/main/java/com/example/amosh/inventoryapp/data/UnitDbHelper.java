989a437ab66c24daf934d95a84e21640d6f384ee
MohamedEmish <emish52020@gmail.com>
2018-11-09 16:25:41 +0200

    9/11

--- app/src/main/java/com/example/amosh/inventoryapp/data/UnitDbHelper.java
+++ app/src/main/java/com/example/amosh/inventoryapp/data/UnitDbHelper.java
@@ -1,12 +1,10 @@
 package com.example.amosh.inventoryapp.data;
 
-import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.net.Uri;
 
 import com.example.amosh.inventoryapp.data.UnitContract.UnitEntry;
 
